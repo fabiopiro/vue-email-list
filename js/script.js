c0b509ed3b835fb61423422288db1a5df8c8c14a
fabiopiro <piroddifabio01@gmail.com>
2021-05-27 14:55:29 +0200

    end (no bonus)

---
+++ js/script.js
@@ -0,0 +1,43 @@
+/*
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+var app = new Vue (
+
+    {
+        el:"#root",
+
+        data: {
+
+            emails:[],
+
+        },
+
+        mounted: function () {
+
+            for (let i = 0; i < 10; i++) {
+
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(
+                    (result) => {
+    
+                        // console.log(result);
+                        
+                        this.emails.push(result.data.response);
+                        console.log(this.emails);
+    
+                    }
+                )
+            }
+        },
+
+        methods:"",
+    }
+
+);
